af2192466e2279550a236b64c2a9f92b64c89f39
RVxLab <code@rvx.works>
2025-06-07 14:08:19 +1000

    Add SQLite optimizations and Nuno Maduro's Essentials package

---
+++ app/Providers/SqliteServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+final class SqliteServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        if (DB::getDriverName() === 'sqlite') {
+            try {
+                DB::unprepared(<<<'SQL'
+                    PRAGMA busy_timeout = 5000;
+                    PRAGMA cache_size = -20000;
+                    PRAGMA foreign_keys = ON;
+                    PRAGMA incremental_vacuum;
+                    PRAGMA mmap_size = 2147483648;
+                    PRAGMA temp_store = MEMORY;
+                    PRAGMA synchronous = NORMAL;
+                SQL,
+                );
+            } catch (QueryException $e) {
+                throw_unless(str_contains($e->getMessage(), 'does not exist.'), $e);
+            }
+        }
+    }
+}
